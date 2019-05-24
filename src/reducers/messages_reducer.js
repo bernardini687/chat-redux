eea486acf5bfdd6202a36dd473b81024bd144d5d
Oscar Bernardini <bernardini687@gmail.com>
2019-05-24 15:46:59 +0200

    Fetch messages OK

--- src/reducers/messages_reducer.js
+++ src/reducers/messages_reducer.js
@@ -7,7 +7,7 @@ export default function(state, action) {
 
   switch (action.type) {
     case FETCH_MESSAGES:
-      return action.payload;
+      return action.payload.messages;
     default:
       return state;
   }
